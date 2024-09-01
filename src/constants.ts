e7358f90cea5ee0c8e047689849a22750ad98057
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-01 11:27:10 +0200

    fix: update db folder location

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 import path from 'node:path';
 
-export const DB_DIR_PATH = path.resolve(__dirname, 'db');
+export const DB_DIR_PATH = path.resolve(__dirname, '../db');
 export const DB_FILE_PATH = path.resolve(DB_DIR_PATH, 'data.json');
