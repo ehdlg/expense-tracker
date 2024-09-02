355cc1bba3e7b261da13c06f66f9668fa06e32a8
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-02 08:35:00 +0200

    feat(actions): add listExpenses action

--- src/actions.ts
+++ src/actions.ts
@@ -1,4 +1,5 @@
 import { getData, saveData } from './utils';
+import { getBorderCharacters, table } from 'table';
 import type { Options, Expense } from './types';
 
 export const addExpense = async (options: Options) => {
@@ -14,4 +15,28 @@ export const addExpense = async (options: Options) => {
   await saveData(data);
 
   console.log(`Expense added suffesfully (ID: ${newExpense.id})`);
+};
+
+export const listExpenses = async () => {
+  const data = await getData();
+
+  if (data.length == 0) {
+    console.log('There are no expenses yet. Use add [options] to add a new expense.');
+
+    process.exit(0);
+  }
+
+  const expenses = [
+    ['ID', 'Description', 'Amount', 'Date'],
+    ...data.map((expense) => {
+      return [expense.id, expense.description, expense.amount, expense.date];
+    }),
+  ];
+
+  console.log(
+    table(expenses, {
+      header: { content: 'Expense List' },
+      border: getBorderCharacters('norc'),
+    })
+  );
 };
