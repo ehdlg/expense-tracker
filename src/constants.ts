dcabfbbb8cf62ddcd12e1e6f007942ce46b8ae58
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-02 15:54:50 +0200

    feat(constants): add Month enum

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,18 @@ import path from 'node:path';
 export const DB_DIR_PATH = path.resolve(__dirname, '../db');
 export const DB_FILE_PATH = path.resolve(DB_DIR_PATH, 'data.json');
 export const DEFAULT_YEAR = new Date().getFullYear();
+
+export enum Month {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
