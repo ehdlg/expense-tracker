a9c83ccc3078b3e4d628015e04151dc1ac5fbe59
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-02 15:33:12 +0200

    feat(constants): add default year constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ import path from 'node:path';
 
 export const DB_DIR_PATH = path.resolve(__dirname, '../db');
 export const DB_FILE_PATH = path.resolve(DB_DIR_PATH, 'data.json');
+export const DEFAULT_YEAR = new Date().getFullYear();
